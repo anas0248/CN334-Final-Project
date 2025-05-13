17f1fac71d9c2243cc45bbae0e1458e41ba5495b
Panuwichhh <panuwich2747@gmail.com>
2025-05-14 05:31:32 +0700

    re

--- app_frontend/next.config.mjs
+++ app_frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_PRODUCT_API_URL: process.env.NEXT_PUBLIC_PRODUCT_API_URL,
+    NEXT_PUBLIC_USER_API_URL: process.env.NEXT_PUBLIC_USER_API_URL,
+  },
 };
 
 export default nextConfig;
