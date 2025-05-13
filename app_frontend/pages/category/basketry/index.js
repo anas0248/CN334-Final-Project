acfd0603bc82e0bba8d6ea532968154ee3ede7bf
Panuwichhh <panuwich2747@gmail.com>
2025-05-14 04:57:43 +0700

    render

--- app_frontend/pages/category/basketry/index.js
+++ app_frontend/pages/category/basketry/index.js
@@ -85,7 +85,7 @@ export default function Basketry() {
     return <p className="text-center py-8">กำลังโหลดเครื่องประดับ...</p>;
   }
 
-  if (error) { <>
+  if (error) { 
     return <p className="text-center py-8 text-red-500">เกิดข้อผิดพลาดในการโหลดเครื่องประดับ</p>;
 
   }
